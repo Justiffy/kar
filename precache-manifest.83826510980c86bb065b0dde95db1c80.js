82d1a72b83a3d6380a31e7fe1d162dddec4f4bba
Listar <shnap2010@yandex.ru>
2020-06-02 21:34:45 +0500

    Updates

---
+++ precache-manifest.83826510980c86bb065b0dde95db1c80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33e2e43080c4f1c0c3397872b6573d93",
+    "url": "/kar/index.html"
+  },
+  {
+    "revision": "790575c56b41b0c21b65",
+    "url": "/kar/static/css/main.00997221.chunk.css"
+  },
+  {
+    "revision": "9256d603f3df90a37f4d",
+    "url": "/kar/static/js/2.97fb8eb8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kar/static/js/2.97fb8eb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "790575c56b41b0c21b65",
+    "url": "/kar/static/js/main.d78455b9.chunk.js"
+  },
+  {
+    "revision": "d1af732588b107cc2cec",
+    "url": "/kar/static/js/runtime-main.daed4073.js"
+  }
+]);
